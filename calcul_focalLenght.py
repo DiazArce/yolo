8555ea5c2ff7053357ae7c797dd21867a6945537
DiazArce <telemek10@gmail.com>
2020-03-05 11:26:38 +0100

    Update calcul_focalLenght.py

--- calcul_focalLenght.py
+++ calcul_focalLenght.py
@@ -1,50 +1,58 @@
 # -*- coding: utf-8 -*-
 """
-Created on Wed Mar  4 12:55:46 2020
+Created on Thu Mar  5 11:17:56 2020
 
 @author: diazd
 """
+
 import numpy as np
 import cv2
 
 
-
-def distance_to_camera(f, R, r):
+def distance_to_camera(knownWidth, focalLength, picWidth):
         # compute and return the distance from the maker to the camera
-        if (r!=0):
-            distance_cm = (f * R) / r
-            distance_m = distance_cm/100
-            return(distance_cm)
-        else:
-            print("division by zero");
+        return (knownWidth * focalLength) / picWidth
+    
+    
 cap = cv2.VideoCapture(0)
+# Check if camera opened successfully
+if (cap.isOpened()== False): 
+  print("Error opening video stream or file")
 
+# Read until video is completed
+while(cap.isOpened()):
+  # Capture frame-by-frame
+  ret, frame = cap.read()
+  if ret == True:
 
-while(True):
-    # Capture frame-by-frame
-    ret, frame = cap.read()
-    
-    cv2.imshow('frame', frame)
-   
-    marker = cv2.selectROI("Frame", frame, fromCenter=False, showCrosshair=False)
+    # Display the resulting frame
+    cv2.imshow('Frame',frame)
+    marker = cv2.selectROI("frame", frame, fromCenter=False, showCrosshair=False)
     print(marker)
-    print("r",marker[2])
-    r = marker[2]
-  
-    print("r: ",r)
+    print(marker[2])
+    picWidth = marker[2]
+    
+    # initialize the known distance from the camera to paper
+    knownDistance = 180.0
+    
     # initialize the known paper width
-    R = 75.0
-    print("R: ",R)
+    knownWidth = 75.0
+    
+    focalLength = (picWidth * knownDistance) / knownWidth
+    print("distance focal:",focalLength)
+    
+    
+    print(distance_to_camera(knownWidth, focalLength, picWidth))
+    # Press Q on keyboard to  exit
+    if cv2.waitKey(25) & 0xFF == ord('q'):
+      break
 
-    f = 512.32
-    
-    print("distance: ",distance_to_camera(f, R, r))
- 
-    
-    
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+  # Break the loop
+  else: 
+    break
 
-# When everything done, release the capture
+# When everything done, release the video capture object
 cap.release()
+
+# Closes all the frames
 cv2.destroyAllWindows()
